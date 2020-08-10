113a601792d3df363591479067c16279be208967
cwcomstock <cwcomstock@ymail.com>
2020-08-10 15:41:31 -0600

    Basic Project Ready

---
+++ js/quotes.js
@@ -0,0 +1,54 @@
+/* Array of Quote Objects
+    quote and source property are manditory
+    citation and year are optional
+*/
+const quotes = [{
+        quote: "Music in the soul can be heard by the universe.",
+        source: "Lao Tzu",
+        citation: "",
+        year: "",
+        category: "Philosophers"
+    },
+    {
+        quote: "Strive not to be a success, but rather to be of value.",
+        source: "Albert Einstein",
+        citation: "",
+        year: "",
+        category: "Scientists"
+    },
+    {
+        quote: "Every child is an artist.  The problem is how to remain an artist once he grows up.",
+        source: "Pablo Picasso",
+        citation: "",
+        year: "",
+        category: "Artists"
+    },
+    {
+        quote: "Everything has beauty, but not everyone can see.",
+        source: "Confucius",
+        citation: "",
+        year: "",
+        category: "Philosophers"
+    },
+    {
+        quote: "People say nothing is impossible, but I do nothing every day.",
+        source: "A. A. Milne",
+        citation: "Winnie-the-Pooh",
+        year: "",
+        category: "Book Characters"
+    },
+    {
+        quote: "'I Love you.', 'I know.'",
+        source: "Princess Leia & Han Solo",
+        citation: "Star Wars: The Empire Strikes Back",
+        year: "1980",
+        category: "Movie Characters"
+    },
+    {
+        quote: "A man who trusts everyone is a fool and a man who trusts no one is a fool. We are all fools if we live long enough.",
+        source: "Robert Jordan",
+        citation: "Winter's Heart: Book Nine of 'The Wheel of Time'",
+        year: "2010",
+        category: "Authors"
+    }
+]
--- js/script.js
+++ js/script.js
@@ -3,31 +3,41 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
-
-
 /***
  * `getRandomQuote` function
-***/
-
-
+ *  1) Generate a random number between 0 and # of quotes
+ *  2) Use the random number and box notation to grab a random item from the ​quotes array
+ *  3) Return the random item
+ ***/
+const getRandomQuote = () => {
+  let randomNumber = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNumber];
+};
 
 /***
  * `printQuote` function
-***/
+ *  1) Generate Random Quote
+ *  2) Create HTML for the quote
+ *  3) Replace Current Quote
+ ***/
+const printQuote = () => {
+  const randomQuote = getRandomQuote();
 
+  let html = `<p class="quote"> ${randomQuote.quote} </p>
+              <p class="source"> ${randomQuote.source}`;
+  if (randomQuote.citation) {
+    html += `<span class="citation"> ${randomQuote.citation} </span>`;
+  }
+  if (randomQuote.year) {
+    html += `<span class="year"> ${randomQuote.year} </span>`;
+  }
+  html += `</p>`;
 
+  document.getElementById('quote-box').innerHTML = html;
+}
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
-
+ ***/
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
